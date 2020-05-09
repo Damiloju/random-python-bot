70fcafb28ef8f946dda98ace6872c309b1cf6214
Yusuf Damiloju <yusufdamiloju@gmail.com>
2020-05-09 10:35:56 +0100

    add start command and enable logging

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 from telegram.ext import Updater, CommandHandler
+import logging
 import re
 import requests
 from dotenv import load_dotenv
+from telegram.ext import MessageHandler, Filters
+
+
 load_dotenv()
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 
 def get_url():
@@ -24,16 +30,34 @@ def get_image_url():
     return url
 
 
-def bop(bot, update):
+def bop(update, context):
     url = get_image_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def start(update, context):
+    chat_id = update.effective_chat.id
+    text = "I can send you random cute dog images!, just send /bop command."
+    context.bot.send_message(
+        chat_id, text)
+
+
+def echo(update, context):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    context.bot.send_message(
+        chat_id, text)
 
 
 def main():
-    updater = Updater(BOT_TOKEN)
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    # updater = Updater(BOT_TOKEN)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop', bop))
+    dp.add_handler(CommandHandler('start', start))
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dp.add_handler(echo_handler)
     updater.start_polling()
     updater.idle()
 
